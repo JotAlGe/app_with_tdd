62aa7322c22090cde7bf555d0ed397397c54f475
Juanchis <juanchismo10@gmail.com>
2022-11-09 22:24:58 -0300

    bootstrap layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,9 +1,43 @@
-<form action="{{route('login')}}" method="POST">
-    <label for="email">Name:</label>
-    <input type="email" name="email" id="email">
+<!DOCTYPE html>
+<html lang="en">
 
-    <label for="password">Password:</label>
-    <input type="text" name="password" id="password">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Login</title>
 
-    <input type="submit" value="Log in">
-</form>
+        {{-- links bootstrap --}}
+        @include('partials.links')
+    </head>
+
+    <body>
+        <h1 class="text-center display-2">Login</h1>
+        <div class="row">
+
+            <div class="col-md-4"></div>
+            <div class="col-md-4">
+                <form action="{{route('login')}}" method="POST">
+
+                    <div class="form-group">
+                        <label for="email">Name:</label>
+                        <input type="email" name="email" id="email" class="form-control form-control-lg">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="password">Password:</label>
+                        <input type="text" name="password" id="password" class="form-control form-control-lg">
+                    </div>
+
+                    <div class="form-group mt-4">
+                        <input type="submit" value="Log in" class="btn btn-info">
+                    </div>
+
+                </form>
+
+            </div>
+            <div class="col-md-4"></div>
+        </div>
+
+    </body>
+
+</html>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,32 +1,72 @@
-<form action="{{ route('register') }}" method="post">
-    @csrf
-    <label>
-        Name:
-        <input type="text" name="name">
-        @error('name')
-        {{ $message }}
-        @enderror
-    </label>
-    <label>
-        Email:
-        <input type="email" name="email">
-        @error('email')
-        {{ $message }}
-        @enderror
-    </label>
-    <label>
-        Password:
-        <input type="password" name="password">
-        @error('password')
-        {{ $message }}
-        @enderror
-    </label>
-    <label>
-        Password Confirmation:
-        <input type="password" name="password_confirmation">
-        @error('password_confirmation')
-        {{ $message }}
-        @enderror
-    </label>
-    <button type="submit">Register</button>
-</form>
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Register</title>
+
+        {{-- links --}}
+        @include('partials.links')
+    </head>
+
+    <body>
+        <h1 class="text-center display-2">Register</h1>
+        <div class="row">
+            <div class="col-md-4"></div>
+            <div class="col-md-4">
+                <form action="{{ route('register') }}" method="post">
+                    @csrf
+                    <div class="form-group">
+                        <label>
+                            Name:
+                            <input type="text" name="name" class="form-control form-control-lg">
+                            @error('name')
+                            <span class="text-danger">
+                                {{ $mesage }}
+                            </span>
+                            @enderror
+                        </label>
+                    </div>
+
+                    <div class="form-group">
+                        <label>
+                            Email:
+                            <input type="email" name="email" class="form-control form-control-lg">
+                            @error('email')
+                            <span class="text-danger">{{ $message }}</span>
+                            @enderror
+                        </label>
+
+                    </div>
+
+                    <div class="form-group">
+                        <label>
+                            Password:
+                            <input type="password" name="password" class="form-control form-control-lg">
+                            @error('password')
+                            <span class="text-danger">{{ $message }}</span>
+                            @enderror
+                        </label>
+                    </div>
+
+                    <div class="form-group">
+                        <label>
+                            Password Confirmation:
+                            <input type="password" name="password_confirmation" class="form-control form-control-lg">
+                            @error('password_confirmation')
+                            <span class=" text-danger">{{ $message }}</span>
+                            @enderror
+                        </label>
+
+                    </div>
+                    <div class="form-group mt-3">
+                        <button type="submit" class="btn btn-info">Register</button>
+                    </div>
+                </form>
+            </div>
+            <div class="col-md-4"></div>
+        </div>
+    </body>
+
+</html>
